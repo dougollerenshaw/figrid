625ef9b78fdf377677c8c336023db672e9593a4d
Doug Ollerenshaw <dougo@alleninstitute.org>
2020-10-23 18:20:57 -0700

    add sample notebook

---
+++ mpl_figure_formatter/__init__.py
@@ -0,0 +1,113 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+def place_axes_on_grid(
+        fig,
+        dim=[1, 1],
+        xspan=[0, 1],
+        yspan=[0, 1],
+        wspace=None,
+        hspace=None,
+        sharex=False,
+        sharey=False,
+        frameon=True):
+    '''
+    Takes a figure with a gridspec defined and places an array of sub-axes on a portion of the gridspec
+    DRO
+    Takes as arguments:
+        fig: figure handle - required
+        dim: number of rows and columns in the subaxes - defaults to 1x1
+        xspan: fraction of figure that the subaxes subtends in the x-direction (0 = left edge, 1 = right edge)
+        yspan: fraction of figure that the subaxes subtends in the y-direction (0 = top edge, 1 = bottom edge)
+        wspace and hspace: white space between subaxes in vertical and horizontal directions, respectively
+    returns:
+        subaxes handles
+    '''
+
+    outer_grid = gridspec.GridSpec(100, 100)
+    inner_grid = gridspec.GridSpecFromSubplotSpec(
+        dim[0],
+        dim[1],
+        subplot_spec=outer_grid[int(100 * yspan[0]):int(100 * yspan[1]), int(100 * xspan[0]):int(100 * xspan[1])],
+        wspace=wspace,
+        hspace=hspace
+    )
+
+    # NOTE: A cleaner way to do this is with list comprehension:
+    # inner_ax = [[0 for ii in range(dim[1])] for ii in range(dim[0])]
+    inner_ax = dim[0] * [dim[1] * [fig]]  # filling the list with figure objects prevents an error when it they are later replaced by axis handles
+    inner_ax = np.array(inner_ax)
+    idx = 0
+    for row in range(dim[0]):
+        for col in range(dim[1]):
+            if row > 0 and sharex == True:
+                share_x_with = inner_ax[0][col]
+            else:
+                share_x_with = None
+
+            if col > 0 and sharey == True:
+                share_y_with = inner_ax[row][0]
+            else:
+                share_y_with = None
+
+            inner_ax[row][col] = plt.Subplot(
+                fig,
+                inner_grid[idx],
+                sharex=share_x_with,
+                sharey=share_y_with,
+                frameon=frameon,
+            )
+
+            if row == dim[0] - 1 and sharex == True:
+                inner_ax[row][col].xaxis.set_ticks_position('bottom')
+            elif row < dim[0] and sharex == True:
+                plt.setp(inner_ax[row][col].get_xtick)
+
+            if col == 0 and sharey == True:
+                inner_ax[row][col].yaxis.set_ticks_position('left')
+            elif col > 0 and sharey == True:
+                plt.setp(inner_ax[row][col].get_yticklabels(), visible=False)
+
+            fig.add_subplot(inner_ax[row, col])
+            idx += 1
+
+    inner_ax = np.array(inner_ax).squeeze().tolist()  # remove redundant dimension
+    return inner_ax
+
+def scalebar(axis, x_pos, y_pos, x_length=None, y_length=None, x_text=None, y_text=None, x_buffer=0.25, y_buffer=0.25, scalebar_color='black', text_color='black', fontsize=10, linewidth=3):
+
+    if x_length is not None:
+        axis.plot(
+            [x_pos,x_pos + x_length],
+            [y_pos,y_pos],
+            color = scalebar_color,
+            linewidth = linewidth
+        )
+        axis.text(
+            x_pos + x_length/2, 
+            y_pos - y_buffer,
+            x_text,
+            color = text_color,
+            fontsize = fontsize,
+            ha = 'center',
+            va = 'top'
+        )
+    
+    if y_length is not None:
+        axis.plot(
+            [x_pos,x_pos],
+            [y_pos,y_pos + y_length],
+            color = scalebar_color,
+            linewidth = linewidth
+        )
+    
+        axis.text(
+            x_pos - x_buffer, 
+            y_pos + y_length/2,
+            y_text,
+            color = text_color,
+            fontsize = fontsize,
+            ha='right',
+            va='center'
+        )
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='mpl_figure_formatter',
+    version='0.0.1',
+    description='Formats multipanel figures',
+    url='https://github.com/dougollerenshaw/mpl_figure_formatter',
+    author='Doug Ollerenshaw',
+    author_email="dougo@alleninstitute.org",
+    license='MIT',
+    install_requires=[
+    "matplotlib",
+    "numpy",
+    ],
+)
