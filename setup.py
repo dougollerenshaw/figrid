85da541494f9ecfa9b6e803752600800d7594905
dougollerenshaw <d.ollerenshaw@gmail.com>
2021-03-02 21:27:16 -0800

    updating package name

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
-setup(name='mpl_figure_formatter',
+setup(name='figrid',
     version='0.0.1',
     description='Formats multipanel figures',
-    url='https://github.com/dougollerenshaw/mpl_figure_formatter',
+    url='https://github.com/dougollerenshaw/figrid',
     author='Doug Ollerenshaw',
     author_email="dougo@alleninstitute.org",
     license='MIT',
     install_requires=[
-    "matplotlib",
-    "numpy",
-    "seaborn"
+        "matplotlib",
+        "numpy",
+        "seaborn"
     ],
 )
