006653d1de5e4f1e7f1580d894acb0f797a38467
dougollerenshaw <d.ollerenshaw@gmail.com>
2025-02-23 14:21:23 -0800

    Updated python versions in setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,11 @@ setup(name='figrid',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13'
     ],
 )
