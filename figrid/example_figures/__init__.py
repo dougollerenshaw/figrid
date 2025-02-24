56013879448af361f5ca84971fc1fa5198cc93f8
dougollerenshaw <d.ollerenshaw@gmail.com>
2025-02-23 16:35:05 -0800

    Black and flake8 format fixes

--- figrid/example_figures/__init__.py
+++ figrid/example_figures/__init__.py
@@ -4,57 +4,61 @@ import seaborn as sns
 
 import figrid as fg
 
+
 def heatmap(axis):
-    '''
+    """
     plot a random pixel image
     input: a single axis handle
-    '''
-    axis.imshow(np.random.randn(100,100),cmap='gray')
-    axis.set_title('An image')
-    axis.axis('off')
-    
+    """
+    axis.imshow(np.random.randn(100, 100), cmap="gray")
+    axis.set_title("An image")
+    axis.axis("off")
+
     # add a scalebar
     fg.scalebar(
-        axis = axis,
-        x_pos = 10,
-        y_pos = 85,
-        x_length = 30, 
-        x_text = '60 um',
-        scalebar_color='white',
-        text_color='white',
+        axis=axis,
+        x_pos=10,
+        y_pos=85,
+        x_length=30,
+        x_text="60 um",
+        scalebar_color="white",
+        text_color="white",
         fontsize=12,
-        y_buffer = -2,
+        y_buffer=-2,
     )
-    
+
+
 def sinusoids(axis):
-    '''
+    """
     plot 3 sinusoids plus a scalebar
     input: a 3 row by 1 column array of axis handles
-    '''
-    t = np.arange(0,10,0.01)
+    """
+    t = np.arange(0, 10, 0.01)
     for row in range(3):
-        f = 0.8*(row + 1)
-        axis[row].plot(t, np.sin(2*np.pi*f*t), color='black',linewidth=2)
-        axis[row].axis('off')
-        axis[row].set_xlim(-0.5,10.5)
-        axis[row].set_ylim(-1.35,1.05)
-        axis[row].set_title('frequency = {:0.1f} Hz'.format(f))
-    
+        f = 0.8 * (row + 1)
+        axis[row].plot(t, np.sin(2 * np.pi * f * t), color="black", linewidth=2)
+        axis[row].axis("off")
+        axis[row].set_xlim(-0.5, 10.5)
+        axis[row].set_ylim(-1.35, 1.05)
+        axis[row].set_title("frequency = {:0.1f} Hz".format(f))
+
     # add a scalebar
     fg.scalebar(
-        axis = axis[2],
-        x_pos = -0.25, 
-        y_pos = -1.25, 
-        x_length = 1, 
-        y_length = 1, 
-        x_text = '1 s', 
-        y_text = '1 u'
+        axis=axis[2],
+        x_pos=-0.25,
+        y_pos=-1.25,
+        x_length=1,
+        y_length=1,
+        x_text="1 s",
+        y_text="1 u",
     )
-    
+
+
 def violins(axis):
-    '''
-    violinplot example from https://seaborn.pydata.org/examples/simple_violinplots.html
-    '''
+    """
+    violinplot example from:
+    https://seaborn.pydata.org/examples/simple_violinplots.html
+    """
     # Create a random dataset across several variables
     rs = np.random.default_rng(0)
     n, p = 40, 8
@@ -64,15 +68,17 @@ def violins(axis):
     # Show each distribution with both violins and points
     sns.violinplot(data=d, palette="light:g", inner="points", orient="h", ax=axis)
     sns.despine()
-    
+
+
 def scatterplot(axis):
-    '''
-    scatterplot example from https://seaborn.pydata.org/examples/layered_bivariate_plot.html
-    '''
+    """
+    scatterplot example from:
+    https://seaborn.pydata.org/examples/layered_bivariate_plot.html
+    """
     # Simulate data from a bivariate Gaussian
     n = 10000
     mean = [0, 0]
-    cov = [(2, .4), (.4, .2)]
+    cov = [(2, 0.4), (0.4, 0.2)]
     rng = np.random.RandomState(0)
     x, y = rng.multivariate_normal(mean, cov, n).T
 
